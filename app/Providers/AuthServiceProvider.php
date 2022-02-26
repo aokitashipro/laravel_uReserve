0d7be3ef7ee4a87c32e1a6570bc3b45bf29fae35
aokitashipro <webmocha2@gmail.com>
2022-02-26 11:04:02 +0900

    gate

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,15 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('admin', function($user){
+            return $user->role === 1;
+        });
+        Gate::define('manager-higher', function($user){
+            return $user->role > 0 && $user->role <= 5;
+        });
+        Gate::define('user-higher', function($user){
+            return $user->role > 0 && $user->role <= 9;
+        });
+        
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,21 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
+Route::prefix('manager')
+->middleware('can:manager-higher')
+->group(function(){
+    Route::get('index', function () {
+        dd('manager');
+    });    
+});
+
+Route::middleware('can:user-higher')
+->group(function(){
+    Route::get('index', function () {
+        dd('user');
+    });
+});
+
 Route::controller(LivewireTestController::class)
 ->prefix('livewire-test')->name('livewire-test.')->group(function(){
     Route::get('index', 'index')->name('index');
